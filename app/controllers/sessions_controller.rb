3ff5b5f677d0f81a56156a48074982875fea0e40
PrincetonJRose <decoyzpr@gmail.com>
2019-06-04 17:59:34 -0400

    Added .env file with password. Set up session to check env file. Env file is in gitignore. Yay! ^_^

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(email: params[:email])
         if @user and @user.authenticate(params[:password])
-            jwt_user = JWT.encode({ user_id: @user.id }, "flatiron!", "HS256")
+            jwt_user = JWT.encode({ user_id: @user.id }, ENV["JWT_AUTH_KEY"], "HS256")
             render json: { jwt_user: jwt_user }, status: :ok
         else
             render json: { errors: "Incorrect email or password." }, status: :unauthorized
