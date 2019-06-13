5308cecfde91b7928095185fa7c9cf6a710cf7c7
PrincetonJRose <decoyzpr@gmail.com>
2019-06-13 00:19:03 -0400

    Touched up controllers a bit for registration form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save!
-            render json: { flash: "Account created!!!" }, status: :ok
+            render json: @user.to_json(include: [:likes, :cocktails, :reviews=>{include: :comments}]), status: :created
         else
             render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password, :first_name, :last_name, :birthdate, :bio)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :birthdate, :bio, :password_digest)
     end
 
 end
