11d4760097f1d580ffb87bf0015cc46bd79aa868
PrincetonJRose <decoyzpr@gmail.com>
2019-06-17 20:14:27 -0400

    Worked on cocktail controller to create new cocktails.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,16 +13,19 @@ class CocktailsController < ApplicationController
     end
 
     def create
-        @cocktail = Cocktail.new
-        if cocktail_params[:ingredients].length > 0 or cocktail_params[:measurements].length > 0
-            @cocktail.name = cocktail_params[:name]
-            @cocktail.videoUrl = cocktail_params[:videoUrl]
-            @cocktail.imageUrl = cocktail_params[:imageUrl]
-            @cocktail.glass = cocktail_params[:glass]
-            @cocktail.instructions = cocktail_params[:instructions]
-            @cocktail.user_id = current_user.id
-            @cocktail.alcoholic = cocktail_params[:alcoholic]
-
+        @cocktail = Cocktail.new(cocktail_params)
+        @cocktail.user_id = current_user.id
+        if params[:cocktail_ingredients].length > 0
+            if @cocktail.save
+                params[:cocktail_ingredients].each do |i|
+                    @ci = CocktailIngredient.new(user_id: @cocktail.user_id, ingredient_id: i[:ingredient], measurement: i[:measurement])
+                    @ci.save
+                    render json: @cocktails.to_json(include: [:ingredients, :cocktail_ingredients]), status: :ok
+            else
+                render json: { errors: @cocktail.errors.full_messages }, status: :unprocessable_entity
+            end
+        elsif params[:cocktail_ingredients].length > 15
+            render json: { errors: ["You must have less than 15 ingredients."] }, status: :unprocessable_entity
         else
             render json: { errors: ["You must have ingredients and measurements for them."] }, status: :unprocessable_entity
         end
@@ -39,7 +42,7 @@ class CocktailsController < ApplicationController
     private
 
     def cocktail_params
-        params.require(:cocktail).permit(:user_id, :name, :instructions, :imageUrl, :videoUrl, :glass, :alcoholic, :category, :ingredients, :measurements)
+        params.require(:cocktail).permit(:user_id, :name, :instructions, :imageUrl, :videoUrl, :glass, :alcoholic, :category, :cocktail_ingredients)
     end
     
 end
