b4388e99d82d139405bc8e76e1851a5559fd8c14
PrincetonJRose <decoyzpr@gmail.com>
2019-06-18 23:48:34 -0400

    Changed create method back to what I first had it working as.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,19 +15,6 @@ class CocktailsController < ApplicationController
     def create
         @cocktail = Cocktail.new(cocktail_params)
         @cocktail.user_id = current_user.id
-        save_cocktail(@cocktail, params)
-    end
-
-    def update
-        @cocktail = Cocktail.find(params[:id])
-        save_cocktail(@cocktail, params)
-    end
-
-    def destroy
-
-    end
-
-    def save_cocktail(cocktail, params)
         if params[:ingredients].length > 0
             if cocktail.save
                 for index in 0...params[:ingredients].length
@@ -44,6 +31,34 @@ class CocktailsController < ApplicationController
         else
             render json: { errors: ["You must have ingredients and measurements for them."] }, status: :unprocessable_entity
         end
+    end
+
+    def update
+        @cocktail = Cocktail.find(params[:id])
+        if params[:ingredients].length > 0
+            if cocktail.save
+                for index in 0...params[:ingredients].length
+                    @ci = CocktailIngredient.new(cocktail_id: cocktail.id, ingredient_id: params[:ingredients][index][:id], measurement: params[:measurements][index])
+                    @ci.save
+                end
+                @cocktails = Cocktail.all
+                    render json: @cocktails.to_json(include: [:ingredients, :cocktail_ingredients]), status: :ok
+            else
+                render json: { errors: @cocktail.errors.full_messages }, status: :unprocessable_entity
+            end
+        elsif params[:ingredients].length > 15
+            render json: { errors: ["You must have less than 15 ingredients."] }, status: :unprocessable_entity
+        else
+            render json: { errors: ["You must have ingredients and measurements for them."] }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+
+    end
+
+    def save_cocktail(cocktail, params)
+
     end
 
     private
