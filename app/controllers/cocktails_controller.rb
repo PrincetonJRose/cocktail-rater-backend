6f94faf0468b88bade871136e48700ee471c859e
PrincetonJRose <decoyzpr@gmail.com>
2019-06-18 23:46:48 -0400

    Can add cocktails using form and it saves mostly correctly.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,35 +15,41 @@ class CocktailsController < ApplicationController
     def create
         @cocktail = Cocktail.new(cocktail_params)
         @cocktail.user_id = current_user.id
-        if params[:cocktail_ingredients].length > 0
-            if @cocktail.save
-                params[:cocktail_ingredients].each do |i|
-                    @ci = CocktailIngredient.new(user_id: @cocktail.user_id, ingredient_id: i[:ingredient], measurement: i[:measurement])
-                    @ci.save
-                end
-                    render json: @cocktails.to_json(include: [:ingredients, :cocktail_ingredients]), status: :ok
-            else
-                render json: { errors: @cocktail.errors.full_messages }, status: :unprocessable_entity
-            end
-        elsif params[:cocktail_ingredients].length > 15
-            render json: { errors: ["You must have less than 15 ingredients."] }, status: :unprocessable_entity
-        else
-            render json: { errors: ["You must have ingredients and measurements for them."] }, status: :unprocessable_entity
-        end
+        save_cocktail(@cocktail, params)
     end
 
     def update
-
+        @cocktail = Cocktail.find(params[:id])
+        save_cocktail(@cocktail, params)
     end
 
     def destroy
 
     end
 
+    def save_cocktail(cocktail, params)
+        if params[:ingredients].length > 0
+            if cocktail.save
+                for index in 0...params[:ingredients].length
+                    @ci = CocktailIngredient.new(cocktail_id: cocktail.id, ingredient_id: params[:ingredients][index][:id], measurement: params[:measurements][index])
+                    @ci.save
+                end
+                @cocktails = Cocktail.all
+                    render json: @cocktails.to_json(include: [:ingredients, :cocktail_ingredients]), status: :ok
+            else
+                render json: { errors: @cocktail.errors.full_messages }, status: :unprocessable_entity
+            end
+        elsif params[:ingredients].length > 15
+            render json: { errors: ["You must have less than 15 ingredients."] }, status: :unprocessable_entity
+        else
+            render json: { errors: ["You must have ingredients and measurements for them."] }, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def cocktail_params
-        params.require(:cocktail).permit(:user_id, :name, :instructions, :imageUrl, :videoUrl, :glass, :alcoholic, :category, :cocktail_ingredients)
+        params.require(:cocktail).permit(:user_id, :name, :instructions, :imageUrl, :videoUrl, :glass, :alcoholic, :category, :cocktail_ingredients, :ingredients, :measurements)
     end
     
 end
