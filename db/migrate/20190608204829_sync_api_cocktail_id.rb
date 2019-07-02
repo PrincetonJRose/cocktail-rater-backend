17debe383fcac81c5bb2e3facbd6bb61fd1760cf
PrincetonJRose <decoyzpr@gmail.com>
2019-07-02 15:44:39 -0400

    Changed migration os sync to fix error on deploy.

--- db/migrate/20190608204829_sync_api_cocktail_id.rb
+++ db/migrate/20190608204829_sync_api_cocktail_id.rb
@@ -1,18 +1,18 @@
 class SyncApiCocktailId < ActiveRecord::Migration[5.2]
   def change
 
-    remove_column :cocktails, :api_drink_id
-    add_column :cocktails, :api_cocktail_id, :integer
+    # remove_column :cocktails, :api_drink_id
+    # add_column :cocktails, :api_cocktail_id, :integer
 
-    remove_column :likes, :drink_id
-    add_column :likes, :cocktail_id, :integer
-    remove_column :likes, :api_drink_id
-    add_column :likes, :api_cocktail_id, :integer
+    # remove_column :likes, :drink_id
+    # add_column :likes, :cocktail_id, :integer
+    # remove_column :likes, :api_drink_id
+    # add_column :likes, :api_cocktail_id, :integer
 
-    remove_column :reviews, :api_drink_id
-    add_column :reviews, :api_cocktail_id, :integer
-    remove_column :reviews, :drink_id
-    add_column :reviews, :cocktail_id, :integer
+    # remove_column :reviews, :api_drink_id
+    # add_column :reviews, :api_cocktail_id, :integer
+    # remove_column :reviews, :drink_id
+    # add_column :reviews, :cocktail_id, :integer
 
   end
 end
